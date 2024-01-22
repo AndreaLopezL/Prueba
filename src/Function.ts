fc2efd3f877f4edf4354ce132b04303b49f44054
AndreaLopez <andyl100804@Gmail.com>
2024-01-22 15:03:35 -0600

    Function

--- src/Function.ts
+++ src/Function.ts
@@ -1,5 +1,7 @@
-const Array1 = [
-    {name: "Emiliano", edad: 25, ID: "2023601135"},
-    {name: "Miriam", edad: 22, ID: "2023604172"},
-    {name: "Josué", edad: 24, ID: "2023601135"},
-]
+
+function suma (a: number,b: number){
+
+    console.log(a + b)
+}
+
+suma(3,4)
