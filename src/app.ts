8840b08afc60ce2519c0432a1cbc922e856bd081
AndreaLopez <andyl100804@Gmail.com>
2024-01-22 13:53:24 -0600

    first commit

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+(()=>{
+ const Var1:string= "Hola";
+ const Var2:number= 2;
+
+ const Array1:any= [ Var1, Var2];
+
+ const Array2:[string, number]= [ Var1, Var2]; 
+
+ const Array3:number[]= [2,3];
+
+ const Array4:Array<string>= ["Hola"];
+ 
+
+
+})
